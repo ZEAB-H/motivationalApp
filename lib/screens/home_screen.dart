2595438de1d02e21f8e6478fa4522ffe5055804b
abel-dejene <abelhailemichael30@gmail.com>
2022-04-01 10:27:33 +0300

    this is the first version of the app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,32 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:flutter_app/screens/second_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  String quote = "";
+  String author = "";
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () async {
-              var url = Uri.parse(
-                  'https://goquotes-api.herokuapp.com/api/v1/random?count=1');
-              var response = await http.get(url);
-              print('Response status: ${response.statusCode}');
-              print('Response body: ${response.body}');
-              var data = jsonDecode(response.body);
-              print(data["quotes"][0]["text"]);
-              print(data["quotes"][0]["author"]);
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => SecondScreen()));
-            },
-            child: Text("Next Quote"),
-          ),
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  quote,
+                  style: TextStyle(
+                    fontSize: 20,
+                    // backgroundColor: Color.fromARGB(255, 144, 132, 89)
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 10.0),
+              child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: Text(
+                    " - $author",
+                    style: TextStyle(fontSize: 18),
+                  )),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                var url = Uri.parse(
+                    'https://goquotes-api.herokuapp.com/api/v1/random?count=1');
+                var response = await http.get(url);
+                print('Response status: ${response.statusCode}');
+                print('Response body: ${response.body}');
+                var data = jsonDecode(response.body);
+                quote = data["quotes"][0]["text"];
+                author = data["quotes"][0]["author"];
+
+                setState(() {});
+              },
+              child: Text("Get Quote !!!"),
+            ),
+          ],
         ),
       ),
     );
